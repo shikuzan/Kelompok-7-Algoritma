804519d2d62611c3eaeb1f65fd46f8702430359b
shikuzan <asyrafzahid07@gmail.com>
2026-05-18 13:55:21 +0800

    Update Shortest_path_from_1_to_n.cpp

--- Shortest_path_from_1_to_n.cpp
+++ Shortest_path_from_1_to_n.cpp
@@ -2,9 +2,8 @@
 using namespace std;
 
 int minimumStep(int n) {
-        // complete the function here
-         int count = 0;
-        while(n > 1){
+        int count = 0; //Jumlah langkah/jarak ke n
+        while(n >= 1){
             if(n % 3 == 0) n /= 3;
             else n--;
             count++;
@@ -12,7 +11,7 @@ int minimumStep(int n) {
         return count;
     }
 int main(){
-    int n;
+    int n; //Jumlah node
     cout << "Input N: ";
     cin >> n;
     cout << "Shortest path = " << minimumStep(n);
